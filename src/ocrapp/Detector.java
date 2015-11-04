8d3101a50c552b73151f0d7d49587dc59f8699d8
John Fish <john@johnafish.ca>
2015-11-04 09:16:09 -0500

    Fields and constructor for Detector class

--- src/ocrapp/Detector.java
+++ src/ocrapp/Detector.java
@@ -1,9 +1,18 @@
 package ocrapp;
 
+import java.awt.image.BufferedImage;
+
 /**
  *
  * @author John Fish <john@johnafish.ca>
  */
 public class Detector {
+    String content;
+    BufferedImage image;
+    BufferedImage[] letterImages;
+    double[][][] minimizedLetters;
     
+    public Detector(BufferedImage i){
+        this.image = i;
+    }
 }
