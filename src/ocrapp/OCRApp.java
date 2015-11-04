630f04d0e0d493ef47e55857bed852cb556e8af4
John Fish <john@johnafish.ca>
2015-11-04 09:37:37 -0500

    Main app setup

--- src/ocrapp/OCRApp.java
+++ src/ocrapp/OCRApp.java
@@ -1,13 +1,31 @@
 package ocrapp;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
 /**
  * @author John Fish <john@johnafish.ca>
  */
 
-public class OCRApp {
+public class OCRApp extends JFrame {
 
+    public OCRApp(){
+        this.setSize(800, 600);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setTitle("OCR App");
+        this.setVisible(true);
+    }
+    
+    @Override
+    public void paint(Graphics g){
+        //Background colour
+        g.setColor(Color.white);
+        g.fillRect(0, 0, 800, 600);
+    }
+    
     public static void main(String[] args) {
-
+        OCRApp app = new OCRApp();
     }
 
 }
