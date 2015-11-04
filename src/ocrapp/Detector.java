15a7388bbd07e6e58cd4b5323fcfc7bbbd6901b5
John Fish <john@johnafish.ca>
2015-11-04 09:22:36 -0500

    Basic detector methods

--- src/ocrapp/Detector.java
+++ src/ocrapp/Detector.java
@@ -15,4 +15,20 @@ public class Detector {
     public Detector(BufferedImage i){
         this.image = i;
     }
+    
+    public void isolateLetters(){
+        //Isolate letters here and populate in letterImages
+    }
+    
+    public void minimizeLetters(){
+        //Convert isolated letter images into 2D arrays of 5x5 of binary values
+    }
+    
+    public void guessLetters(){
+        //Populate content string with guesses from database
+    }
+    
+    public void populateDB(String correct){
+        //Populate the database with the letters and their minimized information
+    }
 }
