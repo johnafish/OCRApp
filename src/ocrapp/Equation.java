2866cd7a9aae01a0b522c59d3718ede24db60172
iLoveJohnFish <ranyodh_mandur@hotmail.com>
2015-11-04 09:41:43 -0500

    added methods/ fields :100:  :100: :100: :laughing: :laughing:

--- src/ocrapp/Equation.java
+++ src/ocrapp/Equation.java
@@ -2,14 +2,27 @@ package ocrapp;
 
 /**
  *
- * @author John Fish <john@johnafish.ca>
+ * @author John Fish <john@johnafish.ca>, Ryan Mandur
  */
 public class Equation {
     double a,b,c;
     int degree;
+    double[] roots;
     String equation;
     
     public Equation(String e){
         this.equation = e;
+    }
+    
+    void getRoots(){
+        //solves for the roots of this equation
+    }
+    
+    void getCoefficients(){
+        //finds coefficients and sets a, b ,c values accordingly 
+    }
+    
+    void getDegrees(){
+        //returns a degree of either 1 or 2
     }
 }
