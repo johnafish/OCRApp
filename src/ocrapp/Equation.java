26678efc2e9768d8c5ea5dae8b423003e8112a79
John Fish <john@johnafish.ca>
2015-11-04 09:10:23 -0500

    Added fields and constructor for Equation class :smiling_imp:

--- src/ocrapp/Equation.java
+++ src/ocrapp/Equation.java
@@ -5,5 +5,9 @@ package ocrapp;
  * @author John Fish <john@johnafish.ca>
  */
 public class Equation {
-    
+    double a,b,c;
+    String equation;
+    public Equation(String e){
+        this.equation = e;
+    }
 }
