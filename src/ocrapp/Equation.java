f1f228f5447d7f62c5f1a08492459d1063f5cdf2
iLoveJohnFish <ranyodh_mandur@hotmail.com>
2015-11-08 17:45:03 -0500

    rewrote getCoefficient method, it's more flexible now

--- src/ocrapp/Equation.java
+++ src/ocrapp/Equation.java
@@ -1,4 +1,4 @@
-package ocrapp;
+package test;
 
 /**
  *
@@ -24,7 +24,7 @@ public class Equation {
         //pretty sure this works
         positive = (double) ( (-b + Math.sqrt( (b*b) - 4*a*c) ) / 2*a );
         negative = (double) ( (-b - Math.sqrt( (b*b) - 4*a*c) ) / 2*a );
-        
+        System.out.println(-b);
         both[0] = negative;
         both[1] = positive;
         
@@ -50,28 +50,26 @@ public class Equation {
     }
     //finds coefficients and sets a, b ,c values accordingly 
     void getCoefficients(){
-        String newEquation = this.equation;
-        //doing this all assuming the string taken has no spaces and 0=,y= are on the left
-        if( newEquation.substring(0, 1).equals("y") ){ //for the form y=mx+b
-            this.a = Double.parseDouble( this.equation.substring(2, 3));
-            this.b = Double.NaN;
-            this.c = Double.parseDouble( this.equation.substring(5));
-            
-        }
-        else{
-            newEquation = newEquation.substring(2); // cuts off 0=, y=, etc.
-            int indexOfX = newEquation.indexOf("x");
-
-            if( newEquation.substring(0, 1).equals("x") )
-                this.a = 1;
-            else
-                this.a = Double.parseDouble( newEquation.substring(indexOfX - 1, indexOfX) ); //for regular form
-            if( newEquation.substring(3, 4).equals("x") )
-                this.b = 1;
-            else
-                this.b = Double.parseDouble( newEquation.substring(indexOfX + 3, indexOfX + 4) );
-         
-            this.c = Double.parseDouble( newEquation.substring(newEquation.length() - 1) );
+        char[] newEquation = this.equation.toCharArray();
+        for (int i = 0; i < newEquation.length; i++) {
+            if( newEquation[i] == 'x' ){
+                if( this.a >= 1){
+                    try{
+                        this.b = Double.parseDouble( String.valueOf(newEquation[i - 1]) );
+                    }
+                    catch( Exception e ){
+                        this.b = 1;
+                    }
+                }
+                try{
+                    this.a = Double.parseDouble( String.valueOf(newEquation[i - 1]) );
+                }
+                catch( Exception e){
+                    this.a = 1;
+                }
+            }
+            else if( i == (newEquation.length - 1) )
+                this.c = Double.parseDouble( String.valueOf(newEquation[i]) );
         }
     }
     //sets a degree of either 1 or 2
