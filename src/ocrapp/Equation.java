5a144ceb0b2e21902a240f78be9773313f511577
iLoveJohnFish <ranyodh_mandur@hotmail.com>
2015-11-08 22:17:11 -0500

    OKAY THIS SHOULD WORK.

--- src/ocrapp/Equation.java
+++ src/ocrapp/Equation.java
@@ -1,4 +1,4 @@
-package test;
+package ocrapp;
 
 /**
  *
@@ -24,7 +24,7 @@ public class Equation {
         //pretty sure this works
         positive = (double) ( (-b + Math.sqrt( (b*b) - 4*a*c) ) / 2*a );
         negative = (double) ( (-b - Math.sqrt( (b*b) - 4*a*c) ) / 2*a );
-        System.out.println(-b);
+ 
         both[0] = negative;
         both[1] = positive;
         
@@ -32,10 +32,8 @@ public class Equation {
     }
     //solves for the roots of this equation
     void getRoots(){
-        double x;
         if( this.degree == 1){
-            x = (double)( -this.c / this.a ); //assuming b is 0
-            roots[0] = x;
+            roots[0] = (double)( -this.c / this.a ); //assuming b is 0
             roots[1] = Double.NaN;
         }
         //checks for real roots
@@ -51,26 +49,42 @@ public class Equation {
     //finds coefficients and sets a, b ,c values accordingly 
     void getCoefficients(){
         char[] newEquation = this.equation.toCharArray();
+        char sign = '+';
         for (int i = 0; i < newEquation.length; i++) {
+           
+            if( newEquation[i] == '-' || newEquation[i] == '+') //checks for sign
+                sign = newEquation[i];
+            
             if( newEquation[i] == 'x' ){
-                if( this.a >= 1){
+                if( this.a > 0){ //for quadratic w two x's
                     try{
-                        this.b = Double.parseDouble( String.valueOf(newEquation[i - 1]) );
+                        this.b = Double.parseDouble( sign + String.valueOf(newEquation[i - 1]) );
                     }
                     catch( Exception e ){
                         this.b = 1;
+                        if ( sign == '-')
+                            this.b *= -1;
                     }
                 }
-                try{
-                    this.a = Double.parseDouble( String.valueOf(newEquation[i - 1]) );
-                }
-                catch( Exception e){
-                    this.a = 1;
+                else{
+                    try{
+                        this.a = Double.parseDouble( sign + String.valueOf(newEquation[i - 1]) );
+                    }
+                    catch( Exception e){
+                        this.a = 1;
+                        if ( sign == '-')
+                            this.a *= -1;
+                    }
                 }
             }
-            else if( i == (newEquation.length - 1) )
-                this.c = Double.parseDouble( String.valueOf(newEquation[i]) );
-        }
+            else if( Character.isDigit( newEquation[i] ) &&  newEquation[i] != '0' ){
+                if( newEquation[i + 1] == 'x'){}
+                else if( newEquation[i - 1] == 'x'){}
+                
+                else
+                    this.c = Double.parseDouble( sign + String.valueOf(newEquation[i]) );
+            }
+        }   
     }
     //sets a degree of either 1 or 2
     void getDegrees(){
